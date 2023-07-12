491139646e8fa894e6708bdd1c0d888abec54ef6
lanvd <lanvd2000@gmail.com>
2023-07-12 22:19:52 +0700

    add navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeroComponent } from './hero/hero.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 const routes: Routes = [
-  {path: 'heroes', component: HeroComponent}
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+  {path: 'heroes', component: HeroComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'detail/:id', component: HeroDetailComponent},
 ];
 
 @NgModule({
