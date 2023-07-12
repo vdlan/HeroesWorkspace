0ab41022750d045c8e02de105405e084d9452976
lanvd <lanvd2000@gmail.com>
2023-07-12 21:48:41 +0700

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroComponent } from './hero/hero.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'heroes', component: HeroComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
